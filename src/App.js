da62ce54b39e2879abcdb04a8905e9d8a75cf975
Angelica Marin <angelicamaru@unisabana.edu.co>
2021-10-05 21:35:28 -0500

    Cambios en servicios de estilo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import MenuMain from "./Components/Menu/Menu";
-import classes from "./Components/estilo.module.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 const App = () => {
   return <MenuMain />;
 };
