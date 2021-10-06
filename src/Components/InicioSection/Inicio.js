8a84dc98f2476c5139d23194f8a41ff6c2712851
Carlos Jimenez <carlosjila@unisabana.edu.co>
2021-10-06 10:43:49 -0500

    carrousel

--- src/Components/InicioSection/Inicio.js
+++ src/Components/InicioSection/Inicio.js
@@ -2,13 +2,23 @@ import ServiciosInicio from "./ServicesInicio";
 import QuieneSomos from "./QuienesSomos";
 import Contactanos from "../Contactanos";
 import Footer from "../Footer";
+import classes from '../InicioSection/Inicio.module.css'
+import inmobiliaria from '../Images/Inmobiliaria.png'
 
 const InicioSection = () => {
   return (
-    <div>
-      <div>
-        <img></img>
-        <h2>Encuentra tu inmueble ideal</h2>
+    <div className={classes.contenido}>
+      <div className={classes.contenedor}>
+        <div>
+          <img src={inmobiliaria}></img>
+        </div>
+        <div className={classes.textoimg}>
+          <div className={classes.texto}>
+            <label>Encuentra</label><br></br>
+            <label> tu inmueble</label><br></br>
+            <label>ideal</label>
+          </div>
+        </div>
       </div>
       <div>
         <h3>Nuestros Servicios</h3>
