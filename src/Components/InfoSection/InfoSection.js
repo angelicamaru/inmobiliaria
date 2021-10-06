2967331ad45010089aeda2c954c2cf57234b7616
Carlos Jimenez <carlosjila@unisabana.edu.co>
2021-10-05 19:31:46 -0500

    Texto

--- src/Components/InfoSection/InfoSection.js
+++ src/Components/InfoSection/InfoSection.js
@@ -7,13 +7,18 @@ function InfoSection() {
   return (
     <div className={classes.contenido}>
       <div className={classes.centrar}>
-
         <Image src={escaleras} roundedCircle />
-
         <div className={classes.titulo}>
-
           <label>Todo sobre nosotros</label>
-
+        </div>
+        <div className={classes.texto}>
+          <p>Somos un emprendimiento que cuenta con un gran equipo de trabajo que posee
+            una basta experiencia en el campo inmobiliario, además contamos con las mejores
+            herramientas para ayudarte en todo lo que necesites.
+          </p>
+          <p>Si estas buscando una empresa de desarrollo inmobiliario que te ayude a
+            convertir tus ideas en una realidad, considera a Inmobiliaria, tenemos los conocimientos,
+            las habilidades y el deseo para ayudarte a triunfar.</p>
         </div>
       </div>
     </div>
