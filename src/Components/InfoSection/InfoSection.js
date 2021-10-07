475ba42d95e6cf4430402e48fddfb42f0f8eda95
Angelica Marin <angelicamaru@unisabana.edu.co>
2021-10-07 11:10:59 -0500

    Quite footers restantes y estilos

--- src/Components/InfoSection/InfoSection.js
+++ src/Components/InfoSection/InfoSection.js
@@ -1,10 +1,10 @@
 import React from "react";
 import classes from "./InfoSection.module.css";
-import escaleras from '../Images/Escaleras.jpeg'
-import senior from '../Images/Senior.jpeg'
-import Image from 'react-bootstrap/Image'
+import escaleras from "../Images/Escaleras.jpeg";
+import senior from "../Images/Senior.jpeg";
+import Image from "react-bootstrap/Image";
 import Footer from "../Footer";
-import Contacto from "../Contacto"
+import Contacto from "../Contacto";
 
 function InfoSection() {
   return (
@@ -15,13 +15,18 @@ function InfoSection() {
           <label>Todo sobre nosotros</label>
         </div>
         <div className={classes.texto}>
-          <label>Somos un emprendimiento que cuenta con un gran equipo de trabajo que posee
-            una basta experiencia en el campo inmobiliario, además contamos con las mejores
-            herramientas para ayudarte en todo lo que necesites.
+          <label>
+            Somos un emprendimiento que cuenta con un gran equipo de trabajo que
+            posee una basta experiencia en el campo inmobiliario, además
+            contamos con las mejores herramientas para ayudarte en todo lo que
+            necesites.
           </label>
-          <label>Si estas buscando una empresa de desarrollo inmobiliario que te ayude a
-            convertir tus ideas en una realidad, considera a Inmobiliaria, tenemos los conocimientos,
-            las habilidades y el deseo para ayudarte a triunfar.</label>
+          <label>
+            Si estas buscando una empresa de desarrollo inmobiliario que te
+            ayude a convertir tus ideas en una realidad, considera a
+            Inmobiliaria, tenemos los conocimientos, las habilidades y el deseo
+            para ayudarte a triunfar.
+          </label>
         </div>
       </div>
       <div className={classes.medio}>
@@ -37,18 +42,20 @@ function InfoSection() {
               <label>N. Barbosa</label>
             </div>
             <div className={classes.textocont}>
-              <label>Esta es tu descripción del Miembro del equipo. 
-                Utiliza este espacio para describir brevemente el puesto y las responsabilidades 
-                de esta persona, o incluye una breve biografía suya. Es una gran oportunidad para 
-                resaltar la importancia de esta persona en el equipo.</label>  
+              <label>
+                Esta es tu descripción del Miembro del equipo. Utiliza este
+                espacio para describir brevemente el puesto y las
+                responsabilidades de esta persona, o incluye una breve biografía
+                suya. Es una gran oportunidad para resaltar la importancia de
+                esta persona en el equipo.
+              </label>
             </div>
           </div>
-        </div>                 
+        </div>
       </div>
       <div>
         <Contacto />
       </div>
-      <Footer />
     </div>
   );
 }
