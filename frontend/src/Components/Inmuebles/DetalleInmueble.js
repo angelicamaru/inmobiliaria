a7517d91c4fcb2b8cd49ae2339fcba8dbe3880bd
Carlos Jimenez <carlosjila@unisabana.edu.co>
2021-11-16 14:25:57 -0500

    Google Maps

--- frontend/src/Components/Inmuebles/DetalleInmueble.js
+++ frontend/src/Components/Inmuebles/DetalleInmueble.js
@@ -2,6 +2,7 @@ import { useParams } from "react-router";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import classes from "./Inmueble.module.css";
+import Map from "../Inmuebles/Map"
 
 function DetalleInmueble() {
   let { operacion } = useParams();
@@ -14,18 +15,28 @@ function DetalleInmueble() {
   });
 
   return (
-    <div className={classes.contenido}>
-      <div className={classes.imageDiv}>
-        <img
-          className={classes.image}
-          src={data.imagen}
-          alt="Mi titulo de la imagen"
-        ></img>
+    <div>
+      <div className={classes.contenido}>
+        <div className={classes.imageDiv}>
+          <img
+            className={classes.image}
+            src={data.imagen}
+            alt="Mi titulo de la imagen"
+          ></img>
+        </div>
+        <div className={classes.texto}>
+          <h2 className={classes.nombreService}>{data.name}</h2>
+          <p>Área: {data.area}</p>
+          <p>{data.descripcion}</p>
+        </div>
       </div>
-      <div className={classes.texto}>
-        <h2 className={classes.nombreService}>{data.name}</h2>
-        <p>Área: {data.area}</p>
-        <p>{data.descripcion}</p>
+      <div className={classes.mapa}>
+        <Map
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAYPWtid2EszivVkAX0nugnAeRyElgaPAE"
+          containerElement={<div style={{height: '400px'}}/>}
+          mapElement={<div style={{height:'100%'}}/>}
+          loadingElement={<p>Cargando</p>}
+        />
       </div>
     </div>
   );
